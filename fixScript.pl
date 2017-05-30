eae89ebbd1c1b941d63e0bb186d995e7c4921a9d
erichards52 <edward.richards5252@gmail.com>
2017-05-30 13:12:06 +0100

    Updated Data Return

--- fixScript.pl
+++ fixScript.pl
@@ -37,15 +37,15 @@ while (my $ds = $factory->next_DocSum){
     # print "$data\n";
 #<Summary><Title>Illumina HiSeq 2500 sequencing</Title><Platform instrument_model="Illumina HiSeq 2500">ILLUMINA</Platform><Statistics total_runs="1" total_spots="2458700" total_bases="159815500" total_size="112179603" load_done="true" cluster_name="public"/></Summary><Submitter acc="ERA886772" center_name="" contact_name="European Nucleotide Archive" lab_name="European Nucleotide Archive"/><Experiment acc="ERX2001348" ver="1" status="public" name="Illumina HiSeq 2500 sequencing"/><Study acc="ERP021057" name="Transposon_directed_insertion_site_sequencing_of_ISS1_libraries_of_Streptococcus_equi_recovered_from_ponies_with_strangles"/><Organism taxid="1336" CommonName="Streptococcus equi strain 4067"/><Sample acc="ERS1505374" name="Streptococcus equi strain 4067"/><Instrument ILLUMINA="Illumina HiSeq 2500"/><Library_descriptor><LIBRARY_NAME>NT928753E</LIBRARY_NAME><LIBRARY_STRATEGY>AMPLICON</LIBRARY_STRATEGY><LIBRARY_SOURCE>GENOMIC</LIBRARY_SOURCE><LIBRARY_SELECTION>PCR</LIBRARY_SELECTION><LIBRARY_LAYOUT> <SINGLE/> </LIBRARY_LAYOUT><LIBRARY_CONSTRUCTION_PROTOCOL>TraDIS pre quality controlled</LIBRARY_CONSTRUCTION_PROTOCOL></Library_descriptor><Bioproject>PRJEB19069</Bioproject><Biosample>SAMEA50444668</Biosample>
 #print "$data\n\n\n";    
- if ($data=~/\<Summary><Title\>(.+)\<\/Title\>\<Platform instrument_model\=\"(.+)\"\>(.+)\<\/Platform\>/){
+ if ($data=~/\<Summary><Title\>(.+)\<\/Title\>\<Platform instrument_model\=\"(.+)\"\>(.+)\<\/Platform\>\<Statistics total_runs\=\"(.+)\" total_spots\=\"(.+)\" total_bases\=\"(.+)\" total_size/){
        $seq=$1;
 	#print "$seq";
 	$model=$2;
 	#print "$model";
        $platform=$3;
 	#print "$platform";
-	$runs=$4;
-	print "$runs";
+	$bases=$6;
+	#print "$runs";
        	}
      if($data=~/\<Statistics total_bases\=\"(.+)\"\<\/Statistics\>/){
        $bases=$4;
@@ -56,5 +56,5 @@ while (my $ds = $factory->next_DocSum){
 		
 	
    }
-  # print "\nId $id\nTitle: $title\nPlatform: $platform\nModel: $model\nNumber of Bases: $bases\n";
+   print "\nId $id\nTitle: $seq\nPlatform: $model\nModel: $platform\nNumber of Bases: $bases\n";
 }

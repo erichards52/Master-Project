c1ef8a903b2f7b2eac2805273ada8e0a9fb99551
erichards52 <edward.richards5252@gmail.com>
2017-06-03 21:02:49 +0100

    Updated script for R import

--- fixScript.pl
+++ fixScript.pl
@@ -12,14 +12,14 @@ my $factory = Bio::DB::EUtilities->new(-eutil => 'esearch',
 
 
 # Returns the total number of query hits
-print "Count = ",$factory->get_count,"\n";
+#print "Count = ",$factory->get_count,"\n";
 
 #Creates a variable equal to the number of query hits
 my $count = $factory->get_count;
 
 # Get history in order to search through all hits
 my $hist = $factory->next_History || die;
-print "History returned\n";
+#print "History returned\n";
 
 
 #Botched code, may return to later
@@ -46,6 +46,8 @@ my $factory = Bio::DB::EUtilities->new(-eutil => 'esummary',
 my $retry = 0; my ($retmax, $retstart) = (2000,0);
 
 
+ print "Id,Organism(CommonName),Organism(ScientificName),Organism(TaxID),SampleAcc,CreateDate,UpdateDate,Platform,Model,Bases,Desc,Design\n";
+
 #While retstart is less than the total hit count, iterates
 while ($retstart < $count) {
     $factory->set_parameters(-retmax => $retmax,
@@ -64,11 +66,13 @@ while ($retstart < $count) {
     }
 
 #Iterative code, adds the amount on that was just searched (retmax)
-    print "Retrieved $retstart";
+    #print "Retrieved $retstart";
     $retstart += $retmax;
 
 #Self-explanatory
 close $out;
+
+ 
 		
 #While there are results						
 while (my $ds = $factory->next_DocSum){
@@ -90,21 +94,32 @@ $date1=$item->get_content;
     
  if ($data=~/\<Summary><Title\>(.+)\<\/Title\>\<Platform instrument_model\=\"(.+)\"\>(.+)\<\/Platform\>\<Statistics total_runs\=\"(.+)\" total_spots\=\"(.+)\" total_bases\=\"(.+)\" total_size/){
        $seq=$1;
+	$seq =~ s/,//;
 	$model=$2;
+	$model =~ s/,//;
        $platform=$3;
+	$platform =~ s/,//;
 	$bases=$6;
+	$bases =~ s/,//;
        	}
 if ($data=~/\<Study acc\=\"(.+)\" name\=\"(.+)\"\/\>\<Organism taxid\=\"(.+)\" CommonName\=\"(.+)\"\/\>\<Sample acc\=\"(.+)\" name\=\"(.+)\"\/\>\<Instrument/) {
 	$acc=$1;
+	$acc =~ s/,//;
 	$study=$2;
+	$study =~ s/,//;
 	$taxid=$3;
+	$taxid =~ s/,//;
 	$org=$4;
+	$org =~ s/,//;
 	$sampleacc=$5;
+	$sampleacc =~ s/,//;
 	$sciname=$6;
+	$sciname =~ s/,//;
 		}     
    }
 
 #Prints variables for each database submission in the SRA
-  print "\nId\t$id\nDesign:\t$seq\nDesc:\t$study\nOrganism (Common Name):\t$org\nOrganism(Scientific Name):\t$sciname\nOrganism(TaxID):\t$taxid\nSampleAcc:\t$sampleacc\nCreateDate:\t$date\nUpdateDate:\t$date1\nPlatform:\t$model\nModel:\t$platform\nNumber of Bases:\t$bases\n\n";
+ # print "Id	Design	Desc	Organism(CommonName)	Organism(ScientificName)	Organism(TaxID)	SampleAcc	CreateDate	UpdateDate	Platform	Model	Bases\n";
+  print "$id,$org,$sciname,$taxid,$sample,$date,$date1,$model,$platform,$bases,$seq,$study\n";
 }
 }

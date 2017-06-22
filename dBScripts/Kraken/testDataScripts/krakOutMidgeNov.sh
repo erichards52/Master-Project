2431e2a1b673462119f60615cdefbc55ffcb7a17
erichards52 <edward.richards5252@gmail.com>
2017-06-22 14:21:17 +0100

    Added Kraken test scripts

---
+++ dBScripts/Kraken/testDataScripts/krakOutMidgeNov.sh
@@ -0,0 +1,2 @@
+kraken --preload --threads 12 --fastq-input --db ~/KrakDB/HumanVirusBacteria /home4/rich01e/KrakDB/faFiles/midgeNov/midge1-0167e2_S9_L001_R1_001.fastq /home4/rich01e/KrakDB/faFiles/midgeNov/midge1-0167e2_S9_L001_R2_001.fastq 
+
---
+++ dBScripts/Kraken/testDataScripts/krakOuttravChik.sh
@@ -0,0 +1,2 @@
+kraken --preload --threads 12 --fastq-input --db ~/KrakDB/HumanVirusBacteria /home4/rich01e/KrakDB/faFiles/travChik/S18_R1.fq /home4/rich01e/KrakDB/faFiles/travChik/S18_R2.fq
+

75638214ac1b10d39634797b9713e4991fc7af75
erichards52 <edward.richards5252@gmail.com>
2017-07-20 16:36:32 +0100

    Fixing tax tables

--- dBScripts/Kaiju/testDataScripts/kaijuTax.sh
+++
@@ -1,9 +0,0 @@
-/home4/rich01e/kaiju/kaiju/bin/addTaxonNames -u -p -t /home4/rich01e/kaiju/kaiju/kaijudb/nodes.dmp -n /home4/rich01e/kaiju/kaiju/kaijudb/names.dmp -i /home4/rich01e/faFiles/kaijuOutput/kaijuHomoResp.out -o /home4/rich01e/faFiles/kaijuOutput/kaijuHomoResp.names.out
-
-/home4/rich01e/kaiju/kaiju/bin/addTaxonNames -u -p -t /home4/rich01e/kaiju/kaiju/kaijudb/nodes.dmp -n /home4/rich01e/kaiju/kaiju/kaijudb/names.dmp -i /home4/rich01e/faFiles/kaijuOutput/kaijuMidgeNov.out -o /home4/rich01e/faFiles/kaijuOutput/kaijuMidge.names.out
-
-/home4/rich01e/kaiju/kaiju/bin/addTaxonNames -u -p -t /home4/rich01e/kaiju/kaiju/kaijudb/nodes.dmp -n /home4/rich01e/kaiju/kaiju/kaijudb/names.dmp -i /home4/rich01e/faFiles/kaijuOutput/kaijuTravChik.out -o /home4/rich01e/faFiles/kaijuOutput/kaijuTrav.names.out
-
-/home4/rich01e/kaiju/kaiju/bin/addTaxonNames -u -p -t /home4/rich01e/kaiju/kaiju/kaijudb/nodes.dmp -n /home4/rich01e/kaiju/kaiju/kaijudb/names.dmp -i /home4/rich01e/faFiles/kaijuOutput/kaijuSRR062415.out -o /home4/rich01e/faFiles/kaijuOutput/kaijuSRR062415.names.out
-
-/home4/rich01e/kaiju/kaiju/bin/addTaxonNames -u -p -t /home4/rich01e/kaiju/kaiju/kaijudb/nodes.dmp -n /home4/rich01e/kaiju/kaiju/kaijudb/names.dmp -i /home4/rich01e/faFiles/kaijuOutput/kaijuSRR062462.out -o /home4/rich01e/faFiles/kaijuOutput/kaijuSRR062462.names.out
--- ncbiScripts/getSraRatScript.sh
+++
@@ -1,4 +0,0 @@
-mapfile -t myArray < RattusNorvegicusAccNos.txt
-while IFS= read -r line; do wget 'http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=runinfo&term=<$line[uid]>'; done < RattusNorvegicusAccNos.txt
-
-

e6abf63e139077ceae72223ad870ea4f13be4944
erichards52 <edward.richards5252@gmail.com>
2017-07-23 14:30:05 +0100

    Updated CLARK-S scripts and brought master branch up-to-date

--- dBScripts/CLARK/kronaScripts/kronaScriptsClarkl.sh
+++
@@ -1,5 +0,0 @@
-ktImportTaxonomy -o /home4/rich01e/faFiles/clarklOutput/kronaHomoResp.html -m 3 /home4/rich01e/faFiles/clarklOutput/resultsHomoResp.krn
-ktImportTaxonomy -o /home4/rich01e/faFiles/clarklOutput/kronaHomoResp.html -m 3 /home4/rich01e/faFiles/clarklOutput/resultsHomoResp.krn
-ktImportTaxonomy -o /home4/rich01e/faFiles/clarklOutput/kronaHomoResp.html -m 3 /home4/rich01e/faFiles/clarklOutput/resultsHomoResp.krn
-ktImportTaxonomy -o /home4/rich01e/faFiles/clarklOutput/kronaHomoResp.html -m 3 /home4/rich01e/faFiles/clarklOutput/resultsHomoResp.krn
-ktImportTaxonomy -o /home4/rich01e/faFiles/clarklOutput/kronaHomoResp.html -m 3 /home4/rich01e/faFiles/clarklOutput/resultsHomoResp.krn
--- dBScripts/CLARK/testDataScripts/metaClarkSOutAllSamplesFull.sh
+++ dBScripts/CLARK/testDataScripts/metaClarkSOutAllSamplesFull.sh
@@ -1,5 +1,5 @@
-./classify_metagenome.sh -P ~/faFiles/homoResp/1H7_S22_L001_R1.fq ~/faFiles/homoResp/1H7_S22_L001_R2.fq -R ~/faFiles/clarkSOutput/HomoResp.txt
-./classify_metagenome.sh -P /home4/rich01e/faFiles/midgeNov/midge1-0167e2_S9_L001_R1_001.fastq /home4/rich01e/faFiles/midgeNov/midge1-0167e2_S9_L001_R2_001.fastq -R ~/faFiles/clarkSOutput/MidgeNov.txt
-./classify_metagenome.sh -P /home4/rich01e/faFiles/travChik/S18_R1.fq /home4/rich01e/faFiles/travChik/S18_R2.fq -R ~/faFiles/clarkSOutput/travChik.txt
-./classify_metagenome.sh -P /home4/rich01e/faFiles/sraFiles/SRR062415_1.fastq /home4/rich01e/faFiles/sraFiles/SRR062415_2.fastq -R ~/faFiles/clarkSOutput/SRR062415.txt
-./classify_metagenome.sh -P /home4/rich01e/faFiles/sraFiles/SRR062462_1.fastq /home4/rich01e/faFiles/sraFiles/SRR062462_2.fastq -R ~/faFiles/clarkSOutput/SRR062462.txt
+./classify_metagenome.sh -P ~/faFiles/homoResp/1H7_S22_L001_R1.fq ~/faFiles/homoResp/1H7_S22_L001_R2.fq -R ~/faFiles/clarkSOutput/HomoResp.txt --spaced
+./classify_metagenome.sh -P /home4/rich01e/faFiles/midgeNov/midge1-0167e2_S9_L001_R1_001.fastq /home4/rich01e/faFiles/midgeNov/midge1-0167e2_S9_L001_R2_001.fastq -R ~/faFiles/clarkSOutput/MidgeNov.txt --spaced
+./classify_metagenome.sh -P /home4/rich01e/faFiles/travChik/S18_R1.fq /home4/rich01e/faFiles/travChik/S18_R2.fq -R ~/faFiles/clarkSOutput/travChik.txt --spaced
+./classify_metagenome.sh -P /home4/rich01e/faFiles/sraFiles/SRR062415_1.fastq /home4/rich01e/faFiles/sraFiles/SRR062415_2.fastq -R ~/faFiles/clarkSOutput/SRR062415.txt --spaced
+./classify_metagenome.sh -P /home4/rich01e/faFiles/sraFiles/SRR062462_1.fastq /home4/rich01e/faFiles/sraFiles/SRR062462_2.fastq -R ~/faFiles/clarkSOutput/SRR062462.txt --spaced

a05626b84ee91f22dd844b17d6b8ec8f7b284588
erichards52 <edward.richards5252@gmail.com>
2017-07-25 18:12:54 +0100

    Final Update: taxTables

---
+++ lineageRet/FullTaxReturn.sh
@@ -0,0 +1,8 @@
+perl SRR062415taxScriptVir.pl > SRR062415ClarkSVir.txt
+perl SRR062462taxScriptVir.pl > SRR062462ClarkSVir.txt
+perl HomoResptaxScript.pl > HomoRespTax.txt
+perl MidgeNovtaxScript.pl > MidgeNovTax.txt
+perl TravChiktaxScript.pl > TravChikTax.txt
+perl SRR062415taxScript.pl > SRR062415Tax.txt
+perl SRR062462taxScript.pl > SRR062462Tax.txt
+

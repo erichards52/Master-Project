fc0c9c1e2235bc5970a1ba48d8a351d537a0cf20
erichards52 <edward.richards5252@gmail.com>
2017-08-08 17:58:01 +0100

    removed unneeded files

--- stepTwo_Benchmark/viralCheck/CLARK/taxScripts/fullKingdomRet.sh
+++
@@ -1,5 +0,0 @@
-perl HomoResptaxScriptVir.pl > HomoRespClarkVir.txt
-perl MidgeNovtaxScriptVir.pl > MidgeNovClarkVir.txt
-perl SRR062415taxScriptVir.pl > SRR062415ClarkVir.txt
-perl TravChiktaxScriptVir.pl > TravChikClarkVir.txt
-perl SRR062462taxScriptVir.pl > SRR062462ClarkVir.txt
--- stepTwo_Benchmark/viralCheck/Kraken/taxScripts/fullKingdomRet.sh
+++
@@ -1,5 +0,0 @@
-perl HomoResptaxScriptVir.pl > HomoRespKrakVir.txt
-perl MidgeNovtaxScriptVir.pl > MidgeNovKrakVir.txt
-perl SRR062415taxScriptVir.pl > SRR062415KrakVir.txt
-perl TravChiktaxScriptVir.pl > TravChikKrakVir.txt
-perl SRR062462taxScriptVir.pl > SRR062462KrakVir.txt

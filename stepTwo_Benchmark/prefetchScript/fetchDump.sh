df7a90e15cf3dbfda90a6a8c759e7a882190e168
erichards52 <edward.richards5252@gmail.com>
2017-08-08 18:58:53 +0100

    moving scripts

---
+++ stepTwo_Benchmark/prefetchScript/fetchDump.sh
@@ -0,0 +1,5 @@
+prefetch SRR062462
+prefetch SRR062415
+fastq-dump --outdir /home4/rich01e/faFiles/sraFiles/ --split-files /home4/rich01e/ncbi/public/sra/SRR062415.sra
+fastq-dump --outdir /home4/rich01e/faFiles/sraFiles/ --split-files /home4/rich01e/ncbi/public/sra/SRR062462.sra
+

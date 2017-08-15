c769980c6a97bbc04d6c7e90fd36d50c1324e8cf
erichards52 <edward.richards5252@gmail.com>
2017-08-15 13:28:30 +0100

    Updates

--- stepThree_Virome/viromeInfo/metaDataScripts/copySampleFiles.sh
+++ stepThree_Virome/viromeInfo/metaDataScripts/copySampleFiles.sh
@@ -1,2 +1,2 @@
-for file in `cat /home4/rich01e/GitProj/stepThree_Virome/viromeInfo/tsvFiles/SamplesOfInteresttoCopy.tsv`; do cp "$file" ~/gitBackup/; done
+for file in `cat /home4/rich01e/GitProj/stepThree_Virome/viromeInfo/tsvFiles/SamplesOfInteresttoCopy.tsv`; do cp "$file" /home4/rich01e/GitProj/stepThree_Virome/viromeInfo/tsvFiles/samplesOfInterest/; done
 

5ec3fde6f1b87432e7fc3284ff33bf7ca17bf83b
erichards52 <edward.richards5252@gmail.com>
2017-08-16 14:42:38 +0100

    Adding tables

--- stepThree_Virome/outputStore/remove?.sh
+++
@@ -1,4 +0,0 @@
-for i in *?.tsv
-do
-    mv -f "$i" "`echo $i | sed 's/?//'`"
-done
--- stepThree_Virome/viromeInfo/copySamples.sh
+++
@@ -1,2 +0,0 @@
-for file in `cat /home4/rich01e/GitProj/stepThree_Virome/viromeInfo/tsvFiles/SamplesOfInteresttoCopy.tsv`; do mv "$file" ~/gitBackup/; done
-
